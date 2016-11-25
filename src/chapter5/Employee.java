5fb3e1a47aa66729fd953d1ab1b71f9a4e037530
octopusfly <381891675@qq.com>
2016-11-25 20:39:32 +0800

    hihoCoder 1403

--- src/chapter5/Employee.java
+++ src/chapter5/Employee.java
@@ -2,17 +2,17 @@ package chapter5;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Objects;
 
 public class Employee extends Person {
+    private String name;
     private double salary;
     private Date hireDay;
 
-    public Employee(String n, double s, int year,
-            int month, int day) {
+    public Employee(String n, double s, int year, int month, int day) {
         super(n);
         salary = s;
-        GregorianCalendar calendar = new GregorianCalendar(
-                year, month - 1, day);
+        GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
         hireDay = calendar.getTime();
     }
 
@@ -24,6 +24,10 @@ public class Employee extends Person {
         return hireDay;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void raiseSalary(double byPercent) {
         double raise = salary * byPercent / 100;
         salary += raise;
@@ -32,8 +36,39 @@ public class Employee extends Person {
     @Override
     public String getDescription() {
         // TODO Auto-generated method stub
-        return String.format(
-                "an employee with a salary of $%.2f",
-                salary);
+        return String.format("an employee with a salary of $%.2f", salary);
+    }
+
+    public boolean equals(Object otherObject) {
+        // a quack test to see if the objects are identical.
+        if (this == otherObject) {
+            return true;
+        }
+
+        // must return false if the explicit parameter is null.
+        if (otherObject == null) {
+            return false;
+        }
+
+        // if the classes doesn't match, they can't be equal.
+        if (getClass() != otherObject.getClass()) {
+            return false;
+        }
+
+        // now we know otherObject is a non-null Employee.
+        Employee other = (Employee) otherObject;
+
+        // test whether the fields ahve identical values.
+        return Objects.equals(name, other.name) && salary == other.salary
+                && Objects.equals(hireDay, other.hireDay);
+    }
+
+    public int hashCode() {
+        return Objects.hash(name, salary, hireDay);
+    }
+
+    public String toString() {
+        return getClass().getName() + "[name = " + name + ", salary = " + salary + ", hireDay = "
+                + hireDay + "]";
     }
 }
--- src/chapter5/Manager.java
+++ src/chapter5/Manager.java
@@ -4,14 +4,18 @@ public class Manager extends Employee {
     private double bonus;
 
     /**
-     * @param n the employee's name.
-     * @param s the salary.
-     * @param year the hire year.
-     * @param month the hire month.
-     * @param day the hire day.
+     * @param n
+     *            the employee's name.
+     * @param s
+     *            the salary.
+     * @param year
+     *            the hire year.
+     * @param month
+     *            the hire month.
+     * @param day
+     *            the hire day.
      */
-    public Manager(String n, double s, int year,
-            int month, int day) {
+    public Manager(String n, double s, int year, int month, int day) {
         super(n, s, year, month, day);
         bonus = 0;
     }
@@ -23,5 +27,25 @@ public class Manager extends Employee {
 
     public void setBonus(double b) {
         bonus = b;
+    }
+
+    public boolean equals(Object otherObject) {
+        if (!super.equals(otherObject)) {
+            return false;
+        }
+
+        Manager other = (Manager) otherObject;
+
+        // super.equals checked that this and other belong to the same class.
+        return bonus == other.bonus;
+
+    }
+
+    public int hashCode() {
+        return super.hashCode() + 17 * new Double(bonus).hashCode();
+    }
+
+    public String toString() {
+        return super.toString() + "[bonus = " + bonus + "]";
     }
 }
